12e87d0b31dec0a3bc688504f51c74632ca4036d
Jack17kdb <jack@kali.Johnson.com>
2026-04-07 18:08:36 +0300

    deploy to render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/personal-portfolio">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
